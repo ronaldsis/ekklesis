7ec320989fa74235bb4daa6cfd4a02f46ba8899f
lxhiguera <lxhiguera@localhost>
2012-04-10 09:42:01 -0530

    * Se descomentaron los repositorios de maven para que busque dependencias en los mismos. * Se a?adio la dependencia joda-time que sera usada para el manejo de fechas  y timestamp * Se organizaron las dependencias de recursos .css y js para adjuntarlos a las paginas usando plug resources

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,16 +23,17 @@ grails.project.dependency.resolution = {
         mavenCentral()
 
         // uncomment these to enable remote dependency resolution from public Maven repositories
-        //mavenCentral()
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenCentral()
+        mavenLocal()
+        mavenRepo "http://snapshots.repository.codehaus.org"
+        mavenRepo "http://repository.codehaus.org"
+        mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
+     	runtime 'joda-time:joda-time:1.6.2'
+		
         // runtime 'mysql:mysql-connector-java:5.1.16'
     }
 
